06ad2f4957df92a9b8748d97a7e6ab52ef9d2d24
Matheus Italo <haiganbr@gmail.com>
2023-06-04 19:13:10 -0300

    testando os commits

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
-   <div>
-    <Cabecalho1/>
-    
-    <Card/>
-  <MenuItem  />
+  <div>
+    <Cabecalho1 />
+
+    <Card />
+    <MenuItem />
   </div>
 </template>
 
 <script>
-
 import Cabecalho1 from "../components/Cabecalho1.vue";
 import MenuItem from "../components/Menu.vue";
 import Card from "@/components/Card.vue";
-export default {name:"HomeTela", components:{ MenuItem, Cabecalho1,Card} };
+export default { name: "HomeTela", components: { MenuItem, Cabecalho1, Card } };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
