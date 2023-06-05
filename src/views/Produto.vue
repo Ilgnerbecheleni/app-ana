10612817133848d0b763ae65e44ef9b94c06f70b
Matheus Italo <haiganbr@gmail.com>
2023-06-05 20:01:39 -0300

    commit ihm

--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -1,8 +1,36 @@
 <template>
-  <div>
-    <h1>Tela Produto</h1>
+  <div class="produto">
     <router-link to="/">voltar</router-link>
+
+    <h1>Tela Produto</h1>
+
+    <div class="descricao">
+      <h2>Pelúcias coelho e rena</h2>
+      <p>Vendo dois pelúcias, um coelho castanho e uma rena castanho claro,
+        que se encontram em um ótimo estado.
+
+        categoria: pelúcias
+        marca: chicco
+        idade: 1 ano
+        estado: muito bom
+        localização: beja
+        raridade: comum
+        ano: 1998
+        preço: 28€
+      </p>
+    </div>
+    <div>
+      Vendedor
+    </div>
+   
+    <button>CONVERSAR</button>
+
+
+      
+
+
   </div>
+
 </template>
 
 <script>
@@ -17,6 +45,6 @@ export default { name: "ProdutoTela" };
   left: 0;
   background-color: rgba(0, 0, 0, 0.8);
   z-index: 3;
-  /* Outros estilos que você desejar */
+ 
 }
 </style>
