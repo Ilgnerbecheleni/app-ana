dbf5581b72f28cd9221fa444285f371ea9bba6a1
Matheus Italo <haiganbr@gmail.com>
2023-06-09 19:18:49 -0300

    COMMIT FONTS

---
+++ src/components/Produto.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="ProdutoTela">
+    <img src="../assets/img/produto.jpg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProdutoTela",
+};
+</script>
+
+<style>
+.ProdutoTela {
+  width: 100;
+  height: 100;
+}
+</style>
--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -4,40 +4,39 @@
       <router-link to="/">voltar</router-link>
       <h1>IMAGEM BACKGROUND FICARÁ AQUI!</h1>
     </header>
-   
+
     <main class="produto">
       <h2>Pelúcias Coelho e Rena</h2>
 
       <p>
-        Vendo dois pelúcias, um coelho castanho e uma rena castanho claro,
-        que se encontram em um ótimo estado.
+        Vendo dois pelúcias, um coelho castanho e uma rena castanho claro, que
+        se encontram em um ótimo estado.
       </p>
-     
+
       <div class="descricao">
-        <p>
-          categoria: pelúcias
-          marca: chicco
-          idade: 1 ano
-          estado: muito bom
-        </p>
-        <p>
-          localização: beja
-          raridade: comum
-          ano: 1998
-          preço: 28€
-        </p>
+        <p id="cinza">categoria:</p>
+        <p id="preto">pelúcias</p>
+        <p id="cinza">marca:</p>
+        <p id="preto">chicco</p>
+        <p id="cinza">idade:</p>
+        <p id="preto">1 ano</p>
+        <p id="cinza">estado:</p>
+        <p id="preto">muito bom</p>
+        <p id="cinza">localização:</p>
+        <p id="preto">beja</p>
+        <p id="cinza">raridade:</p>
+        <p id="preto">comum</p>
+        <p id="cinza">ano:</p>
+        <p id="preto">1998</p>
+        <p id="cinza">preço:</p>
+        <p id="preto">28€</p>
       </div>
 
-      <div>
-       Vendedor
-      </div>
+      <div>Vendedor</div>
 
       <button>CONVERSAR</button>
     </main>
-
-   
   </div>
-
 </template>
 
 <script>
@@ -54,9 +53,16 @@ export default { name: "ProdutoTela" };
   z-index: 3;
 }
 
-.descricao{
+.descricao {
   display: grid;
   grid-template-columns: 2fr 2fr;
 }
-
+#cinza {
+  font-family: Open sans-serif;
+  color: #aca9a5;
+}
+#preto {
+  font: bold;
+  color: #2f2e2c;
+}
 </style>
