c7e69b8a1ceb6f605fd5db51e02b3eb8175ccb26
ilgner becheleni <ilgner.gui@gmail.com>
2023-06-09 20:28:40 -0300

    commit ilgner HOME

--- src/components/Cabecalho1.vue
+++ src/components/Cabecalho1.vue
@@ -1,9 +1,16 @@
 <template>
-    <nav class="cabecalho1">
-    <h1>Bem-vindo </h1>
-    <h2>nome_utilizador</h2>
-    <input type="text">
-    </nav>
+ 
+<header class="cabecalho1">
+  <nav class="cabecalho-items">
+    <h1 class="title1">Bem vindo</h1>
+   <h2>user_name</h2>
+    <div class="search">
+      <img src="https://cdn-icons-png.flaticon.com/512/2319/2319177.png" alt="" class='searchimg'>
+      <input type="text">
+
+    </div>
+  </nav>
+</header>
 </template>
 
 
@@ -15,11 +22,51 @@ export default {
 
 <style>
 .cabecalho1{
-    z-index: 999;
+    z-index:99 ;
     width: 100%;
     height: 205px;
     background-color: rgb(172, 156, 156);
     position: fixed;
     top: 0;
+    display: flex;
+    align-items: start;
+    padding-left: 0;
 }
+
+.searchimg{
+  position:absolute;
+    left: 3px;
+  cursor: pointer;
+  max-width:20px;
+}
+
+.search{
+  position:relative;
+  margin:0;
+    display: flex;
+  align-items: center;
+}
+
+.search input[type="text"]{
+   padding-left: 30px;
+  height:25px;
+  border-radius:5px;
+  outline:none;
+  border:none;
+}
+.cabecalho-items {
+  margin-left:10px;
+  padding: 10px;
+}
+h2{
+margin-top:0;
+margin-bottom:6px;  
+}
+
+.title1{
+  
+  margin-top:80px;
+  margin-bottom:0;
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,22 @@
     <Cabecalho1 />
     <h2>vistos recentemente</h2>
 <section class="telaHome">
+<div class="wrapper">
   <Card />
     <Card />
     <Card />
+</div>
+
   </section>
+  <h3>Recomendados</h3>
+  <section class="recomendados">
+  <Card />
     <Card />
     <Card />
     <Card />
+    <Card />
+    <Card />
+</section>
 
  
     <MenuItem />
@@ -32,5 +41,17 @@ export default { name: "HomeTela", components: { MenuItem, Cabecalho1, Card } };
   overflow-x: scroll;
   white-space: nowrap;
 }
+.wrapper{
+  display: inline-flex;
+  gap: 10px; /* Espaçamento entre os cards */
+}
 
+.recomendados{
+  display: grid;
+ grid-template-columns: 1fr 1fr;
+ justify-content: space-evenly;
+ align-items: center;
+ margin-left:20px;
+ gap:15px;
+}
 </style>
