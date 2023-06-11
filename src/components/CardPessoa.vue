1ac1b4454a102a3a7d0aeba7c0817dfa70afa228
Matheus Italo <haiganbr@gmail.com>
2023-06-11 14:13:13 -0300

    ATT

--- src/components/CardPessoa.vue
+++ src/components/CardPessoa.vue
@@ -1,39 +1,36 @@
 <template>
-    <section class="card-pessoa">
-      <img src="../assets/img/pessoa.png" alt="pessoa">
-      
-
-    </section>
-        
-    
+  <section class="card-pessoa">
+    <img src="../assets/img/pessoa.png" alt="pessoa" />
+  </section>
 </template>
-
 
 <script>
 export default {
-  name: 'CardItem'
-}
+  name: "CardItem",
+};
 </script>
 
 <style>
 .card-pessoa {
-    font-family: 'Open Sans', sans-serif;
-text-decoration: none;
+  font-family: "Open Sans", sans-serif;
+  text-decoration: none;
   width: 155px;
   height: 245px;
   border-radius: 14px;
-  overflow: hidden; /* Adicionado para esconder o conteúdo que excede as bordas */
-  box-shadow: -3px 10px 22px 6px rgba(0,0,0,0.1);
-  margin:5px;
+  overflow: hidden; /* Adicionado para esconder o conteúdo que excede as bordas! */
+  box-shadow: -3px 10px 22px 6px rgba(0, 0, 0, 0.1);
+  margin: 5px;
 }
 
 .card-pessoa-img {
-
   width: 155px;
   height: 151px;
-   object-fit: cover;
-  mask-image: linear-gradient(to bottom, transparent 50%, white 100%); /* Máscara de corte */
+  object-fit: cover;
+  mask-image: linear-gradient(
+    to bottom,
+    transparent 50%,
+    white 100%
+  ); /* Máscara de corte */
   border-radius: 100%;
 }
-
 </style>
